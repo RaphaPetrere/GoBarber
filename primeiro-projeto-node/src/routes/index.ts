f15df21968ee1718b503ff2363094e45159282ad
Raphael Cardoso <raphaelpetrere@gmail.com>
2021-06-04 20:08:39 -0300

    Limpando o código

--- primeiro-projeto-node/src/routes/index.ts
+++ primeiro-projeto-node/src/routes/index.ts
@@ -5,7 +5,8 @@ const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 /*
-Toda rota que utilizar /appointments, vai repassar oq estiver dps de appointments para esse appointmentsRouter
+Toda rota que utilizar /appointments, vai repassar oq estiver dps de appointments 
+para esse appointmentsRouter.
 Então tipo, vamos supor que vc cria um rota de post para criar usuários
 /appointments/user
 Você passará para o appointmentsRouter que vai utilizar a rota users.
